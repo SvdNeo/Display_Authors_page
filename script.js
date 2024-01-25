00dc62c0f09feac269a0849e38349499d5e76395
SvdNeo <sanjay.d@neoito.com>
2024-01-25 13:08:12 +0530

    using fetch accessed the author info from the database

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
 
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json())
+  .then((data) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
+  })
+  .catch((err) => {
+   authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+  });
+
